031cbf7c1ccdcdb51778386fc1ad8b2a5d27de2f
kutta75 <kutta75@gmail.com>
2023-07-02 18:43:13 +0200

    test compil

---
+++ main/main.c
@@ -0,0 +1,13 @@
+/* test wifi depuis esp 32 */
+/* objectif  mettre l esp32 dans le reseau local et lui faire envoyer des requetes http à un serveur de ce reseau */
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+void app_main(void)
+{
+    while (true) {
+        printf("Hello from app_main!\n");
+        sleep(1);
+    }
+}
