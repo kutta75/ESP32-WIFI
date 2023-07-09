af8ecf84987b6fff9b5ef142236ac0c278c0fd36
kutta75 <kutta75@gmail.com>
2023-07-09 10:06:33 +0200

    maj commentaire

--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 /* test wifi depuis esp 32 */
 /* objectif  mettre l esp32 dans le reseau local et lui faire envoyer des requetes http à un serveur de ce reseau */
 /* 02/07/2023 */
+/* 09/07/2023 */
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
